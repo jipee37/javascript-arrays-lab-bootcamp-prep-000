c6c5c9d686b4a18e8e886924fd99bf89c5d998fc
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-15 12:38:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,13 +32,9 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  return kittens.slice(0, kittens[kittens.length-2])
+  return kittens.slice(0, kittens.length-1) //last elt not included
 }
 
 function removeFirstKitten(){
-  return kittens.slice(1, kittens[kittens.length-1])
+  return kittens.slice(1, kittens.length)
 }
-
-
-
-
